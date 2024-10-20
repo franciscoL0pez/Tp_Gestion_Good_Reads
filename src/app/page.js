1954eb8f09c7d9b7318229209d449c438e5fcc7b
Chavela <angie87wong@gmail.com>
2024-10-20 20:20:35 -0300

    feat: Agrego imagenes en el inicio de la pagina

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,8 @@
+
 "use client"; // Marca el componente como un Client Component
 
 import { useEffect, useState } from "react";
-import { ChakraProvider, Spinner, Box, Stack, Image, Button } from "@chakra-ui/react";
+import { ChakraProvider, Spinner, Box, Stack, Image, Button, Center, Link, SimpleGrid } from "@chakra-ui/react";
 import styles from "./page.module.css";
 
 export default function Home() {
@@ -18,18 +19,20 @@ export default function Home() {
   return (
     <ChakraProvider>
       <div className={styles.page}>
+        {/* Cabecera con el Center */}
+        <Center bg="tomato" h="100px" color="white">
+          <Image
+            src="/images/logo.jpg" 
+            width={180} // Ancho de la imagen
+            height={180} // Alto de la imagen
+            priority // Carga prioritaria
+          />
+        </Center>
+
         <main className={styles.main}>
+          
           {loading ? (
             <>
-            
-              <Image
-                className={styles.logo}
-                src="/images/logo.jpg" // Ruta de la imagen local
-                alt="Descripción de la imagen"
-                width={180}
-                height={180}
-                priority
-              />
               <Spinner
                 size="xl"
                 thickness="4px"
@@ -40,34 +43,60 @@ export default function Home() {
               />
             </>
           ) : (
-            <Box 
-              p={4} 
-              position="relative"
-              bg="white.100"
-              display="flex"
-              justifyContent="center"
-              alignItems="center"
-              width="100%"
-              height={{ base: "20vh", md: "40vh", lg: "20vh" }} // Altura adaptable
-              overflow="hidden"
-              flexDirection="column" // Para centrar verticalmente
-            > {}
-              
-              <Image
-                src="/images/logo.jpg" 
-                width={180} // Ancho de la imagen
-                height={180} // Alto de la imagen
-                priority // Carga prioritaria
-              />
+            <>
+              {/* Sección con 5  imágenes */}
+              <Box p={4} bg="gray.100">
              
-            
-            </Box>
-            
+              <SimpleGrid columns={[2, null, 5]} spacing="40px">
+                <Image 
+                  src="/images/DorianGrey.jpg" 
+                  alt="Dorian Grey" 
+                  borderRadius="md"
+                  boxShadow="md"
+                  transition="transform 0.2s"
+                  _hover={{ transform: "scale(1.05)" }} // Efecto hover
+                />
+                <Image 
+                  src="/images/ElExtranjero.jpg" 
+                  alt="El Extranjero" 
+                  borderRadius="md"
+                  boxShadow="md"
+                  transition="transform 0.2s"
+                  _hover={{ transform: "scale(1.05)" }} 
+                />
+                <Image 
+                  src="/images/LaMetaMorfosis.jpg" 
+                  alt="La Metamorfosis" 
+                  borderRadius="md"
+                  boxShadow="md"
+                  transition="transform 0.2s"
+                  _hover={{ transform: "scale(1.05)" }} 
+                />
+                <Image 
+                  src="/images/MasAlláDelInvierno.jpg" 
+                  alt="Más Allá Del Invierno" 
+                  borderRadius="md"
+                  boxShadow="md"
+                  transition="transform 0.2s"
+                  _hover={{ transform: "scale(1.05)" }} 
+                />
+                <Image 
+                  src="/images/PoesiaCompleta.jpg" 
+                  alt="Poesía Completa" 
+                  borderRadius="md"
+                  boxShadow="md"
+                  transition="transform 0.2s"
+                  _hover={{ transform: "scale(1.05)" }} 
+                />
+              </SimpleGrid>
+              </Box>
+              
+            </>
           )}
         </main>
+        
         {!loading && ( // El footer solo se mostrará si loading es false
           <footer className={styles.footer}>
-            {/* Código del footer va aquí */}
             <Stack spacing={4} mb={4} width="100%" alignItems="center">
               <Button colorScheme="blue" onClick={() => alert("Iniciar Sesión")}>
                 Iniciar Sesión
