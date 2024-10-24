60b92a03e708e88ff137e2a8a11d8ba66ec34c7f
Simon Stein <sstein@fi.uba.ar>
2024-10-24 00:13:23 -0300

    feat: add sidebar menu

---
+++ src/app/home/books/page.js
@@ -0,0 +1,11 @@
+import { Flex, Heading } from "@chakra-ui/react";
+
+export default function BooksPage() {
+  return (
+    <Flex padding={"5%"}>
+      <Heading fontWeight={400} fontSize={"40px"}>
+        Libros
+      </Heading>
+    </Flex>
+  );
+}
